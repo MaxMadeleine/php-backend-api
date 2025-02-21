d1c84ac4cadfeacb3cecd3a9e378b84db0ebda8a
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-02-22 00:35:29 +0100

    CRUD DONE !!

--- index.php
+++ index.php
@@ -3,16 +3,32 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Allowed methods
+header("Access-Control-Allow-Credentials: true"); // Allow credentials (if needed)
 
-echo"The PHP Backend";
+
 
 include 'DbConnect.php';
 $objDb = new DbConnect;
-$conn = $objDb->connect(); //why does this way of syntax
+$conn = $objDb->connect(); 
 
 $method = $_SERVER['REQUEST_METHOD'];
 switch($method) {
+case "GET":
+    $sql = "SELECT * FROM users";
+    $path = explode( '/', $_SERVER["REQUEST_URI"]);
+    if(isset($path[3]) && is_numeric([3])){
+        $sql .= "WHERE id = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $path[3]);
+        $stmt->execute();
+        $users = $stmt->fetch(PDO::FETCH_ASSOC);
+    };
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($users);
+    break;
 case "POST":
     $user = json_decode( file_get_contents('php://input'));
     $sql = "INSERT INTO users(id, name , email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
@@ -30,6 +46,39 @@ case "POST":
    }
    echo json_encode($response);
    break;
-}
+   case "PUT":
+    $user = json_decode( file_get_contents('php://input'));
+    $sql = "UPDATE users SET name =:name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id =:id";
+   $stmt = $conn->prepare($sql);
+   $updated_at = date('Y-m-d');
+   $stmt->bindParam(':id', $user->id);
+   $stmt->bindParam(':name', $user->name);
+   $stmt->bindParam(':email', $user->email);
+   $stmt->bindParam(':mobile', $user->mobile);
+   $stmt->bindParam(':updated_at', $updated_at);
+   if($stmt->execute()){
+    $response = ['status' => 1, 'message' => 'record updated succsesfully'];
+   } else{
+    $response = ['status' => 0, 'message' => 'failed to update record'];
+
+   }
+   echo json_encode($response);
+   break;
+   case "DELETE":
+    $sql = "DELETE FROM users WHERE id = :id";
+    $path = explode( '/', $_SERVER["REQUEST_URI"]);
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id', $path[3]);
+    $stmt->execute();
+    
+    if ($stmt->execute()) {
+    $response = ['status' => 1, 'message' => 'record updated succsesfully'];
+    } else{
+    $response = ['status' => 0, 'message' => 'failed to update record'];
+   }    
+   break;
+
+};
 
 
