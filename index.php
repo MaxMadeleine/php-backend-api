b3f8948410b3a4af98e1586a87441b84e1e2c3d0
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-02-22 13:34:02 +0100

    done crud

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
@@ -7,78 +7,72 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Allo
 header("Access-Control-Allow-Credentials: true"); // Allow credentials (if needed)
 
 
-
 include 'DbConnect.php';
 $objDb = new DbConnect;
-$conn = $objDb->connect(); 
+$conn = $objDb->connect();
 
 $method = $_SERVER['REQUEST_METHOD'];
-switch($method) {
-case "GET":
-    $sql = "SELECT * FROM users";
-    $path = explode( '/', $_SERVER["REQUEST_URI"]);
-    if(isset($path[3]) && is_numeric([3])){
-        $sql .= "WHERE id = :id";
+switch ($method) {
+    case "GET":
+        $sql = "SELECT * FROM users";
+        $path = explode('/', $_SERVER["REQUEST_URI"]);
+        if (isset($path[3]) && is_numeric([3])) {
+            $sql .= "WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        };
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($users);
+        break;
+    case "POST":
+        $user = json_decode(file_get_contents('php://input'));
+        $sql = "INSERT INTO users(id, name , email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
+        $stmt = $conn->prepare($sql);
+        $created_at = date('Y-m-d');
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':created_at', $created_at);
+        if ($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'record created succsesfully'];
+        } else {
+            $response = ['status' => 0, 'message' => 'failed to created record'];
+        }
+        echo json_encode($response);
+        break;
+    case "PUT":
+        $user = json_decode(file_get_contents('php://input'));
+        $sql = "UPDATE users SET name =:name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id =:id";
+        $stmt = $conn->prepare($sql);
+        $updated_at = date('Y-m-d');
+        $stmt->bindParam(':id', $user->id);
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':updated_at', $updated_at);
+        if ($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'record updated succsesfully'];
+        } else {
+            $response = ['status' => 0, 'message' => 'failed to update record'];
+        }
+        echo json_encode($response);
+        break;
+    case "DELETE":
+        $sql = "DELETE FROM users WHERE id = :id";
+        $path = explode('/', $_SERVER["REQUEST_URI"]);
+
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':id', $path[3]);
         $stmt->execute();
-        $users = $stmt->fetch(PDO::FETCH_ASSOC);
-    };
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($users);
-    break;
-case "POST":
-    $user = json_decode( file_get_contents('php://input'));
-    $sql = "INSERT INTO users(id, name , email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
-   $stmt = $conn->prepare($sql);
-   $created_at = date('Y-m-d');
-   $stmt->bindParam(':name', $user->name);
-   $stmt->bindParam(':email', $user->email);
-   $stmt->bindParam(':mobile', $user->mobile);
-   $stmt->bindParam(':created_at', $created_at);
-   if($stmt->execute()){
-    $response = ['status' => 1, 'message' => 'record created succsesfully'];
-   } else{
-    $response = ['status' => 0, 'message' => 'failed to created record'];
 
-   }
-   echo json_encode($response);
-   break;
-   case "PUT":
-    $user = json_decode( file_get_contents('php://input'));
-    $sql = "UPDATE users SET name =:name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id =:id";
-   $stmt = $conn->prepare($sql);
-   $updated_at = date('Y-m-d');
-   $stmt->bindParam(':id', $user->id);
-   $stmt->bindParam(':name', $user->name);
-   $stmt->bindParam(':email', $user->email);
-   $stmt->bindParam(':mobile', $user->mobile);
-   $stmt->bindParam(':updated_at', $updated_at);
-   if($stmt->execute()){
-    $response = ['status' => 1, 'message' => 'record updated succsesfully'];
-   } else{
-    $response = ['status' => 0, 'message' => 'failed to update record'];
-
-   }
-   echo json_encode($response);
-   break;
-   case "DELETE":
-    $sql = "DELETE FROM users WHERE id = :id";
-    $path = explode( '/', $_SERVER["REQUEST_URI"]);
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':id', $path[3]);
-    $stmt->execute();
-    
-    if ($stmt->execute()) {
-    $response = ['status' => 1, 'message' => 'record updated succsesfully'];
-    } else{
-    $response = ['status' => 0, 'message' => 'failed to update record'];
-   }    
-   break;
-
+        if ($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'record updated succsesfully'];
+        } else {
+            $response = ['status' => 0, 'message' => 'failed to update record'];
+        }
+        break;
 };
-
-
